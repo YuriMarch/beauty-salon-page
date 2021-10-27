2451a89c662c8dd0ff53eefbd805131988e076bd
Yuri <ypdmarch@gmail.com>
2021-10-27 23:13:53 +0200

    footer and main.js updated

--- main.js
+++ main.js
@@ -15,16 +15,15 @@ for (const link of links) {
 	});
 }
 
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
-
-window.addEventListener("scroll", function () {
+function changeHeaderWhenScroll() {
+	const header = document.querySelector("#header");
+	const navHeight = header.offsetHeight;
 	if (window.scrollY >= navHeight) {
 		header.classList.add("scroll");
 	} else {
 		header.classList.remove("scroll");
 	}
-});
+}
 
 // CAROUSEL //
 
@@ -35,4 +34,36 @@ const swiper = new Swiper(".swiper", {
 	},
 	mousewheel: true,
 	keyboard: true,
+});
+
+// Scroll Reveal shows the elements when scrolling the page
+
+const scrollReveal = ScrollReveal({
+	origin: "top",
+	distance: "30px",
+	duration: 700,
+	reset: true,
+});
+
+scrollReveal.reveal(
+	`#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .bradn, footer .social`,
+	{ interval: 100 }
+);
+
+// Back to top Button
+
+function backToTop() {
+	const backToTopButton = document.querySelector(".back-to-top");
+	const backToTopHeight = home.offsetHeight;
+
+	if (window.scrollY >= backToTopHeight) {
+		backToTopButton.classList.add("show");
+	} else {
+		backToTopButton.classList.remove("show");
+	}
+}
+
+window.addEventListener("scroll", function () {
+	changeHeaderWhenScroll();
+	backToTop();
 });
